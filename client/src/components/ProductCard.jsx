393fdfbb366df4a2cbb253ae5d91a27973fa227e
Auwalu Muhammad Izziddin <auwalmuhammad5559@gmail.com>
2025-04-30 21:37:52 +0100

    ProductCard

---
+++ client/src/components/ProductCard.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
+
+
+const ProductCard = ({product}) => {
+    const [count, setCount] = React.useState(0);
+    const {currency, addTocart, updateCartItem, removeFromCart, cartItems } = useAppContext();
+  
+     
+
+    return product && (
+        <div className="border border-primary rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
+            <div className="group cursor-pointer flex items-center justify-center px-2">
+                <img className="group-hover:scale-105 transition max-w-26 md:max-w-36" src={product.image[0]} alt="product" />
+            </div>
+            <div className="text-primary text-sm">
+                <p>{product.category}</p>
+                <p className="text-primary font-medium text-lg truncate w-full">{product.name}</p>
+                <div className="flex items-center gap-0.5">
+                    {Array(5).fill('').map((_, i) => (
+                     <img key={i} src={i < 4 ? assets.star_icon : assets.star_dull_icon} alt="" 
+                         className="md:w-3.5 w-3" />
+                        
+                        
+                    ))}
+                    <p>({4})</p>
+                </div>
+                <div className="flex items-end justify-between mt-3">
+                    <p className="md:text-xl text-base">
+                       {currency}${product.offerPrice}{" "} <span className="text-primary md:text-sm text-xs line-through">${product.price}</span>
+                    </p>
+                    <div onClick={(e) => { e.stopPropagation(); }} className="text-primary">
+                        {!cartItems[product._id] ? (
+                            <button 
+                            onClick={() => addTocart(product._id)}
+                            className="flex items-center justify-center gap-1 cursor-pointer bg-primary border border-primary md:w-[80px] w-[64px] h-[34px] rounded" >
+                                <img src={assets.cart_icon}  alt="cart_icon" />
+                                Add
+                            </button>
+                        ) : (
+                            <div className="flex items-center justify-center gap-2 md:w-20 w-16 h-[34px] bg-primary rounded select-none">
+                                <button onClick={() => { removeFromCart(product._id)}} className="cursor-pointer text-md px-2 h-full" >
+                                    -
+                                </button>
+                                <span className="w-5 text-center">{cartItems[product._id]}</span>
+                                <button onClick={() => { addTocart(product._id)}} className="cursor-pointer text-md px-2 h-full" >
+                                    +
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,14 +1,67 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from '../assets/assets'
+import { toast } from 'react-hot-toast';
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
 
+    const currency = import.meta.VITE_CURRENCY;
+
     const navigate = useNavigate();
+    const [isSeller, setIsSeller] = useState(false)
     const [user, setUser] = useState(true);
+    const [products, setProducts] = useState([])
     const [showUserLogin, setShowUserLogin] = useState(false);
-     const value = {navigate, user, setUser, setShowUserLogin, showUserLogin}
+    const [cartItems, setCartItems] = useState({});
+    
+
+    // fetch all products
+    const fetchProducts = async () => {
+        setProducts(dummyProducts)
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    },[])
+
+    // add product to cart
+    const addTocart = (itemId) => {
+        let cartData = structuredClone(cartItems);
+
+        if(cartData[itemId]){
+            cartData[itemId] += 1;
+        }else {
+            cartData[itemId] = 1;
+        }
+        setCartItems(cartData);
+        toast.success("Added to Cart")
+    }
+
+
+    // updated cart
+    const updateCartItem = (itemId, quantity) => {
+        let cardData = structuredClone(cartItems);
+        cartData[itemId] = quantity;
+        setCartItems(cardData)
+        toast.success("Cart Updated");
+    }
+
+    // remove product from cart
+    const removeFromCart = (itemId) => {
+        let cardData = structuredClone(cartItems);
+        if(cardData[itemId]){
+            cardData[itemId] -= 1;
+            if(cardData[itemId] === 0){
+                delete cardData[itemId];
+            }
+        }
+        toast.success("Removed from cart");
+        setCartItems(cardData);
+    }
+    
+    const value = {navigate,cartItems,  user, addTocart, updateCartItem, removeFromCart, setUser, currency, setShowUserLogin, showUserLogin, isSeller, setIsSeller, products, setProducts }
 
     return (
         <AppContext.Provider value={value}>
